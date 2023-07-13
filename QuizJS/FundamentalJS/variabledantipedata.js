cccf6ac1f464d507c33fc86d52c38863f223aac0
Tao <48190353+Taoisty@users.noreply.github.com>
2023-07-14 05:50:09 +0700

    add script >> variabledantipedata.

---
+++ QuizJS/FundamentalJS/variabledantipedata.js
@@ -0,0 +1,15 @@
+/**
+ * TODO:
+ * Buatlah variabel firstName, lastName, age, isMarried dengan ketentuan:
+ *  - firstName: bertipe data string, dengan nilai "John".
+ *  - lastName: bertipe data string, dengan nilai "Doe".
+ *  - age: bertipe data number, dengan nilai 25.
+ *  - isMarried: bertipe data boolean, dengan nilai true.
+ */
+
+// Tulis kode di bawah ini
+
+let firstName = "John"
+let lastName = "Doe"
+const age = 25;
+const isMarried = true;
