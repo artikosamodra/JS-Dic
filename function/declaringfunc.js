8aebf3ee0801cd5da8ecfd570cc7c57e623ce47d
Tao <48190353+Taoisty@users.noreply.github.com>
2023-07-24 11:27:51 +0700

    add script >> declaringfunc.js. -function -function+params -pengembalian nilai function -expressiogn function

--- function/declaringfunc.js
+++ function/declaringfunc.js
@@ -23,3 +23,28 @@ function multiply(a, b) {
 }
 let result = multiply(10, 2)
 console.log(result) //output: 20
+
+//menggunakan return agar nilai function dapat dikembalikan
+function food(yourname, foodname) {
+    if (foodname === "Bakso") {
+        return `${yourname} pesan bakso`;
+    } else if (foodname === "Soto") {
+        return `${yourname} pesan soto`;
+    } else {
+        return `${yourname} pesan seblak`;
+    }
+}
+let foodmessage = food("Artiko", "Soto");
+console.log(foodmessage); //output: Artiko pesan soto
+
+//expression fuction
+const mygreet = function (inName, inLanguage) {
+    if (inLanguage === "Inggris") {
+        return "Good Morning " + inName + "!";
+    } else if (inLanguage === "Perancis") {
+        return "Bonjour " + inName + "!";
+    } else {
+        return "Selamat Pagi " + inName + "!";
+    }
+}
+console.log(mygreet('Artiko', 'Inggris')); //output: Good Morning Artiko!
