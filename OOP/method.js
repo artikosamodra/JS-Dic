92eb80753111049d1980ff1377dc6118909c1075
Tao <48190353+Taoisty@users.noreply.github.com>
2023-08-03 13:57:43 +0700

    membuat method dengan get & set. - get properti chassisNumber dari _chassisNumber. - nilai chassisNumber didapat dari _generateChassisNumber dengan math.floor

--- OOP/method.js
+++ OOP/method.js
@@ -1,12 +1,38 @@
 class Car {
-    constructor(brand, color, maxSpeed, chassisNumber) {
+    constructor(brand, color, maxSpeed) {
         this.brand = brand;
         this.color = color;
         this.maxSpeed = maxSpeed;
-        this.chassisNumber = chassisNumber;
+        this._chassisNumber = this._generateChassisNumber(); //tambahkan ()
+    }
+
+    get chassisNumber() {
+        return this._chassisNumber;
+    }
+
+    set chassisNumber(chassisNumber) {
+        console.log('you are not allowed to change the chassis number');
+    }
+
+    //method
+    drive() {
+        console.log(`${this.brand} ${this.color} is driving`);
+    }
+
+    reserve() {
+        console.log(`${this.brand} ${this.color} is reservering`);
+    }
+
+    turn(direction) {
+        console.log(`${this.brand} ${this.color} is ${direction}`);
+    }
+
+    _generateChassisNumber() {
+        return `${this.brand}-${Math.floor(Math.random() * 1000)}`;
     }
 }
 
+const car = new Car('BMW', 'red', 205);
 const car1 = new Car('BWM', 'red', 200, 'b-1');
 const car2 = new Car('Audi', 'blue', 220, 'a-1');
 const car3 = new Car('BWM', 'black', 250, 'b-2');
@@ -14,3 +40,12 @@ const car3 = new Car('BWM', 'black', 250, 'b-2');
 console.log(car1); //output: Car{brand: 'BWM', color: 'red', maxpSpeed: 200, chassisNumber: 'b-1'}
 console.log(car2); //output: Car{brand: 'Audi', color: 'blue', maxpSpeed: 220, chassisNumber: 'a-1'}
 console.log(car3); //output: Car{brand: 'BWM', color: 'black', maxpSpeed: 250, chassisNumber: 'b-2'}
+
+car1.drive(); //output: BWM red is driving
+car2.reserve(); //output: Audi blue is reservering
+car3.turn(); //output: Audi blue is reservering
+
+console.log(car._chassisNumber); //output:BMW-709 (math.random)
+car._chassisNumber = 'BMW-1'; 
+console.log(car._chassisNumber); //output: BMW-1
+console.log(car._generateChassisNumber()); //output: BMW-39 (math.random)
