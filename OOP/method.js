3570f24b87a9a3d97aabb2362088d3b5f1ebdfd4
Tao <48190353+Taoisty@users.noreply.github.com>
2023-08-03 14:03:59 +0700

    add enclosing untuk mengatasi hak akses private. (#)

--- OOP/method.js
+++ OOP/method.js
@@ -1,13 +1,15 @@
 class Car {
+    #chassisNumber = null; //enclosing
     constructor(brand, color, maxSpeed) {
         this.brand = brand;
         this.color = color;
         this.maxSpeed = maxSpeed;
-        this._chassisNumber = this._generateChassisNumber(); //tambahkan ()
+        this.#chassisNumber = this.#generateChassisNumber(); //tambahkan ()
     }
 
+    //membuat property chassing number dari ._chassisNumber.
     get chassisNumber() {
-        return this._chassisNumber;
+        return this.#chassisNumber;
     }
 
     set chassisNumber(chassisNumber) {
@@ -26,8 +28,8 @@ class Car {
     turn(direction) {
         console.log(`${this.brand} ${this.color} is ${direction}`);
     }
-
-    _generateChassisNumber() {
+    //memberikan nilai pada _chassisNumber dengan math.floor random.
+    #generateChassisNumber() {
         return `${this.brand}-${Math.floor(Math.random() * 1000)}`;
     }
 }
@@ -45,7 +47,9 @@ car1.drive(); //output: BWM red is driving
 car2.reserve(); //output: Audi blue is reservering
 car3.turn(); //output: Audi blue is reservering
 
-console.log(car._chassisNumber); //output:BMW-709 (math.random)
+
+//ketika underscore '_' dirubah tagar '#', maka statusnya enclosing karena bersifat private. ini untuk mengatasi hak akses private pada property dan objek 
+console.log(car.#chassisNumber); //output:BMW-709 (math.random)
 car._chassisNumber = 'BMW-1'; 
-console.log(car._chassisNumber); //output: BMW-1
-console.log(car._generateChassisNumber()); //output: BMW-39 (math.random)
+console.log(car.#chassisNumber); //output: BMW-1
+console.log(car.#generateChassisNumber()); //output: BMW-39 (math.random)
