c837b2d88be09060e37db846f8488683f0e25004
Tao <48190353+Taoisty@users.noreply.github.com>
2023-08-21 04:28:11 +0700

    add script >> reusable.js -Array Map

---
+++ funcProgram/reusable.js
@@ -0,0 +1,5 @@
+ //Array Map
+
+const newArray = ['Harry', 'Ron', 'Jeff', 'Thomas'].map((name) => { return `${name}!` });
+
+console.log(newArray); //output: [ 'Harry!', 'Ron!', 'Jeff!', 'Thomas!' ]
