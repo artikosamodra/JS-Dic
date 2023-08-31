c5353f97a373931c6a068a06ddbd2a0f726be16f
Tao <48190353+Taoisty@users.noreply.github.com>
2023-08-31 16:56:31 +0700

    - menambahkan class validatorError untuk kustomisasi Error sesuai kebutuhan programmer atau kebutuhan program kita.

--- Errorhandling/custom.js
+++ Errorhandling/custom.js
@@ -1,4 +1,11 @@
 //custom error
+class validatorError extends Error { //Error adalah class bawaan dari JS, seperti arr dan calender
+    constructor(message) {
+        super(message);
+        this.name = "Validation Error";
+     }
+}
+
 
 //let json = '{"name": "artiko", "age": 30}';
 let json = '{"age": 30}';
@@ -8,7 +15,11 @@ try {
     let user = JSON.parse(json);
 
     if (!user.name) {
-        throw new SyntaxError("'name' is required.");
+        throw new validatorError("'name' is required."); //'validatorError' merupakan class error yang dibuat secara custom sesuai kebutuhan kita sendiri. 
+    }
+
+    if (!user.age) {
+        throw new validatorError("'age' is required");
     }
 
     console.log(user.name);
@@ -16,6 +27,8 @@ try {
 } catch (error) {
     if (error instanceof SyntaxError) {
         console.log(`JSON Error: ${error.message}`);
+    } else if (error instanceof validatorError) { //bagian ini merupakan kondisi custom error yang kita buat sendiri.
+        console.log(`Invalid data: ${error.message}`);
     } else if (error instanceof ReferenceError) {
         console.log(error.message);
     } else {
