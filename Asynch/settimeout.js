b9862cd4195818525ce9015debb52a0ce211c74b
Tao <48190353+Taoisty@users.noreply.github.com>
2023-09-01 08:42:46 +0700

    add script >> seetimeout.js -memberikan delay pada output.

---
+++ Asynch/settimeout.js
@@ -0,0 +1,7 @@
+console.log('Selamat datang!');
+
+setTimeout(() => {
+    console.log('Terimakasih sudah mampir, silahkan datang kembali');
+}, 3000) //output ini akan didelay selama 3 detik (3000) sebeluh ditampilkan.
+
+console.log('Ada yang bisa dibantu?');
