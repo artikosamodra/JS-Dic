af92db5ac2f952b4c75db906e4b02636231bfff2
Tao <48190353+Taoisty@users.noreply.github.com>
2023-09-09 12:04:46 +0700

    answer Quiz Callback Promise. - dengan menggunakana promisify -dengan menggunakan promise-based

--- QuizJS/QAsynProccess/QCallbacktoPromise.js
+++ QuizJS/QAsynProccess/QCallbacktoPromise.js
@@ -7,8 +7,12 @@
  * - Jika nama fungsinya berubah, sesuaikan nilai yang diekspor tanpa mengubah nama properti di akhir berkas ini.
  */
 
+
+//jika menggunakan promisify
+const { promisify } = require('util');
 function getProvinces(countryId, callback) {
     setTimeout(() => {
+
         if (countryId === 'id') {
             callback(null, [
                 { id: 'id-jk', name: 'Jakarta' },
@@ -22,5 +26,65 @@ function getProvinces(countryId, callback) {
     }, 1000);
 }
 
+const getProvincesPromisify = promisify(getProvinces);
+
+getProvincesPromisify('id')
+    .then(countryId => console.log(countryId))
+    .catch(err => console.error(err.message));
+
+getProvincesPromisify('us')
+    .then(countryId => console.log(countryId))
+    .catch(err => console.error(err.message));
+
+/*output:
+jika benar:
+[
+  { id: 'id-jk', name: 'Jakarta' },
+  { id: 'id-bt', name: 'Banten' },
+  { id: 'id-jr', name: 'Jawa Barat' }
+]
+
+jika salah:
+Country not found
+*/
+//=====================================================
+
+//jika menggunakan promise-based
+function getProv(countryId) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            if (countryId === 'id') {
+                resolve([
+                    { id: 'id-jk', name: 'Jakarta' },
+                    { id: 'id-bt', name: 'Banten' },
+                    { id: 'id-jr', name: 'Jawa Barat' },
+                ]);
+                return;
+            }
+            reject(new Error('Country not found'));
+
+        }, 1000);
+    });
+}
+
+getProv('id')
+    .then(provinces => console.log(provinces))
+    .catch(err => console.log(err.message));
+
+getProv('us')
+    .then(provinces => console.log(provinces))
+    .catch(err => console.error(err.message));
+
+/*output:
+jika benar:
+[
+  { id: 'id-jk', name: 'Jakarta' },
+  { id: 'id-bt', name: 'Banten' },
+  { id: 'id-jr', name: 'Jawa Barat' }
+]
+
+jika salah:
+Country not found
+*/
 
 module.exports = { getProvinces: getProvinces };
